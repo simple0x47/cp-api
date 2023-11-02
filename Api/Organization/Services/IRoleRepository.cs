44c95521b605650741d9d4777cafcbf8290ec58d
Gabriel Amihalachioaie <gabriel@simpleg.eu>
2023-11-02 16:33:54 +0100

    f outdated documentation

--- Api/Organization/Services/IRoleRepository.cs
+++ Api/Organization/Services/IRoleRepository.cs
@@ -6,7 +6,7 @@ namespace Cuplan.Organization.Services;
 public interface IRoleRepository
 {
     /// <summary>
-    ///     Gets the ID of the admin role.
+    ///     Gets the admin role.
     /// </summary>
     /// <returns>An <see cref="Role" /> or an error.</returns>
     public Task<Result<Role, Error<ErrorKind>>> GetAdminRole();

fd9dc62ad5f2e3f3581278ad2f46823a24fea067
Gabriel Amihalachioaie <gabriel@simpleg.eu>
2023-11-03 14:37:31 +0100

    d reason behind the existence of 'WrappedResult'

--- Api/Organization/ControllerModels/WrappedResult.cs
+++ Api/Organization/ControllerModels/WrappedResult.cs
@@ -1,5 +1,10 @@
 namespace Cuplan.Organization.ControllerModels;
 
+/// <summary>
+///     Wraps a specific type so the resulting JSON is contained within an object always.
+///     Required for Javascript code.
+/// </summary>
+/// <typeparam name="TWrapped"></typeparam>
 public class WrappedResult<TWrapped>
 {
     public WrappedResult(TWrapped result)

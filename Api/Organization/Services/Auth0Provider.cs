99c8d63f7d4f45dc3b7c97056d955894e2d9fb97
Gabriel Amihalachioaie <gabriel@simpleg.eu>
2023-11-16 01:12:34 +0100

    + timeout for 'Auth0Provider'

--- Api/Organization/Services/Auth0Provider.cs
+++ Api/Organization/Services/Auth0Provider.cs
@@ -3,6 +3,7 @@ using Core;
 using Core.Secrets;
 using Cuplan.Organization.Models.Authentication;
 using Cuplan.Organization.ServiceModels.Auth0;
+using Organization.Config;
 using LoginPayload = Cuplan.Organization.Models.Authentication.LoginPayload;
 using RefreshTokenPayload = Cuplan.Organization.ServiceModels.Auth0.RefreshTokenPayload;
 using SignUpPayload = Cuplan.Organization.Models.Authentication.SignUpPayload;
@@ -24,128 +25,200 @@ public class Auth0Provider : IAuthProvider
     private readonly string _clientId;
     private readonly string _clientSecret;
     private readonly string _database;
+    private readonly TimeSpan _forgotPasswordTimeout;
     private readonly string _identityProviderAudience;
     private readonly string _identityProviderUrl;
 
-    public Auth0Provider(IConfiguration config, ISecretsManager secretsManager)
+    private readonly ILogger<Auth0Provider> _logger;
+    private readonly TimeSpan _loginTimeout;
+    private readonly TimeSpan _refreshTokenTimeout;
+
+    private readonly TimeSpan _signUpTimeout;
+
+    public Auth0Provider(ConfigurationReader config, ISecretsManager secretsManager, ILogger<Auth0Provider> logger)
     {
         // Expects the identity provider url to finish with '/'.
-        _identityProviderUrl = config["IdentityProvider:Authority"];
-        _identityProviderAudience = config["IdentityProvider:Audience"];
+        _identityProviderUrl = config.GetStringOrThrowException("IdentityProvider:Authority");
+        _identityProviderAudience = config.GetStringOrThrowException("IdentityProvider:Audience");
 
         if (_identityProviderUrl is null) throw new NullReferenceException("'_identityProviderUrl' is null.");
 
-        _clientId = secretsManager.Get(config["Auth0:ClientIdSecret"]);
+        _clientId = secretsManager.Get(config.GetStringOrThrowException("Auth0:ClientIdSecret"));
 
         if (_clientId is null) throw new NullReferenceException("'_clientId' is null.");
 
-        _clientSecret = secretsManager.Get(config["Auth0:ClientSecretSecret"]);
+        _clientSecret = secretsManager.Get(config.GetStringOrThrowException("Auth0:ClientSecretSecret"));
 
         if (_clientSecret is null) throw new NullReferenceException("'_clientSecret' is null.");
 
-        _database = config["Auth0:Database"];
+        _database = config.GetStringOrThrowException("Auth0:Database");
 
         if (_database is null) throw new NullReferenceException("'_database' is null.");
 
         _client = new HttpClient();
+
+        _signUpTimeout = TimeSpan.FromSeconds(config.GetDoubleOrDefault("AuthProvider:SignUpTimeout", 15));
+        _loginTimeout = TimeSpan.FromSeconds(config.GetDoubleOrDefault("AuthProvider:LoginTimeout", 15));
+        _forgotPasswordTimeout =
+            TimeSpan.FromSeconds(config.GetDoubleOrDefault("AuthProvider:ForgotPasswordTimeout", 15));
+        _refreshTokenTimeout = TimeSpan.FromSeconds(config.GetDoubleOrDefault("AuthProvider:RefreshTokenTimeout", 15));
+
+        _logger = logger;
     }
 
     public async Task<Result<string, Error<string>>> SignUp(SignUpPayload signUp)
     {
-        ServiceModels.Auth0.SignUpPayload payload = new();
-        payload.client_id = _clientId;
-        payload.email = signUp.Email;
-        payload.password = signUp.Password;
-        payload.connection = _database;
+        try
+        {
+            ServiceModels.Auth0.SignUpPayload payload = new();
+            payload.client_id = _clientId;
+            payload.email = signUp.Email;
+            payload.password = signUp.Password;
+            payload.connection = _database;
 
-        HttpResponseMessage response =
-            await _client.PostAsync($"{_identityProviderUrl}{SignUpEndpoint}", JsonContent.Create(payload));
+            HttpResponseMessage response =
+                await _client.PostAsync($"{_identityProviderUrl}{SignUpEndpoint}", JsonContent.Create(payload))
+                    .WaitAsync(_signUpTimeout);
 
-        if (response.StatusCode != HttpStatusCode.OK)
-            return await HandleRegisterAuth0ErrorResponse(response);
+            if (response.StatusCode != HttpStatusCode.OK)
+                return await HandleRegisterAuth0ErrorResponse(response);
 
-        SignUpOkResponse? okResponse = await response.Content.ReadFromJsonAsync<SignUpOkResponse>();
+            SignUpOkResponse okResponse = await response.Content.ReadFromJsonAsync<SignUpOkResponse>();
 
-        if (okResponse is null)
-            return Result<string, Error<string>>.Err(new Error<string>(ErrorKind.OkResponseNull,
-                "deserialized ok response is null"));
+            if (okResponse._id is null)
+                return Result<string, Error<string>>.Err(new Error<string>(ErrorKind.OkResponseNull,
+                    "deserialized ok response '_id' is null"));
 
-        // the prefix is added so the ids represented within the tokens are equal to the ones used
-        // within Cuplan.
-        return Result<string, Error<string>>.Ok($"{IdPrefix}{okResponse.Value._id}");
+            // the prefix is added so the ids represented within the tokens are equal to the ones used
+            // within Cuplan.
+            return Result<string, Error<string>>.Ok($"{IdPrefix}{okResponse._id}");
+        }
+        catch (TimeoutException)
+        {
+            return Result<string, Error<string>>.Err(new Error<string>(ErrorKind.TimedOut, "sign up timed out"));
+        }
+        catch (Exception e)
+        {
+            string message = $"failed to sign up: {e}";
+            _logger.LogInformation(message);
+            return Result<string, Error<string>>.Err(new Error<string>(ErrorKind.ServiceError, message));
+        }
     }
 
     public async Task<Result<LoginSuccessPayload, Error<string>>> Login(LoginPayload login)
     {
-        ServiceModels.Auth0.LoginPayload payload = new()
+        try
         {
-            audience = _identityProviderAudience,
-            client_id = _clientId,
-            client_secret = _clientSecret,
-            grant_type = ResourceOwnerGrant,
-            username = login.Email,
-            password = login.Password,
-            scope = RequiredScopes
-        };
+            ServiceModels.Auth0.LoginPayload payload = new()
+            {
+                audience = _identityProviderAudience,
+                client_id = _clientId,
+                client_secret = _clientSecret,
+                grant_type = ResourceOwnerGrant,
+                username = login.Email,
+                password = login.Password,
+                scope = RequiredScopes
+            };
 
-        HttpResponseMessage response =
-            await _client.PostAsync($"{_identityProviderUrl}{LoginEndpoint}", JsonContent.Create(payload));
+            HttpResponseMessage response =
+                await _client.PostAsync($"{_identityProviderUrl}{LoginEndpoint}", JsonContent.Create(payload))
+                    .WaitAsync(_loginTimeout);
 
-        if (response.StatusCode != HttpStatusCode.OK)
-            return await HandleLoginAuth0ErrorResponse(response);
+            if (response.StatusCode != HttpStatusCode.OK)
+                return await HandleLoginAuth0ErrorResponse(response);
 
-        LoginOkResponse? okResponse = await response.Content.ReadFromJsonAsync<LoginOkResponse>();
+            LoginOkResponse okResponse = await response.Content.ReadFromJsonAsync<LoginOkResponse>();
 
-        if (okResponse is null)
-            return Result<LoginSuccessPayload, Error<string>>.Err(new Error<string>(ErrorKind.OkResponseNull,
-                "deserialized ok response is null"));
+            if (okResponse.access_token is null)
+                return Result<LoginSuccessPayload, Error<string>>.Err(new Error<string>(ErrorKind.OkResponseNull,
+                    "deserialized ok response's content is null"));
 
-        LoginSuccessPayload successPayload = (LoginSuccessPayload)okResponse;
+            LoginSuccessPayload successPayload = okResponse;
 
-        return Result<LoginSuccessPayload, Error<string>>.Ok(successPayload);
+            return Result<LoginSuccessPayload, Error<string>>.Ok(successPayload);
+        }
+        catch (TimeoutException)
+        {
+            return Result<LoginSuccessPayload, Error<string>>.Err(new Error<string>(ErrorKind.TimedOut,
+                "login timed out"));
+        }
+        catch (Exception e)
+        {
+            string message = $"failed to login: {e}";
+            _logger.LogInformation(message);
+            return Result<LoginSuccessPayload, Error<string>>.Err(new Error<string>(ErrorKind.ServiceError, message));
+        }
     }
 
     public async Task<Result<Empty, Error<string>>> ForgotPassword(ForgotPasswordPayload forgotPassword)
     {
-        ChangePasswordPayload payload = new()
+        try
         {
-            client_id = _clientId,
-            connection = _database,
-            email = forgotPassword.Email
-        };
+            ChangePasswordPayload payload = new()
+            {
+                client_id = _clientId,
+                connection = _database,
+                email = forgotPassword.Email
+            };
 
-        HttpResponseMessage response = await _client.PostAsync($"{_identityProviderUrl}{ChangePasswordEndpoint}",
-            JsonContent.Create(payload));
+            HttpResponseMessage response = await _client.PostAsync($"{_identityProviderUrl}{ChangePasswordEndpoint}",
+                JsonContent.Create(payload)).WaitAsync(_forgotPasswordTimeout);
 
-        if (response.StatusCode != HttpStatusCode.OK)
-            return Result<Empty, Error<string>>.Err(new Error<string>(ErrorKind.UnknownError,
-                "Auth0 replied with a non-ok response"));
+            if (response.StatusCode != HttpStatusCode.OK)
+                return Result<Empty, Error<string>>.Err(new Error<string>(ErrorKind.UnknownError,
+                    "Auth0 replied with a non-ok response"));
 
-        return Result<Empty, Error<string>>.Ok(new Empty());
+            return Result<Empty, Error<string>>.Ok(new Empty());
+        }
+        catch (TimeoutException)
+        {
+            return Result<Empty, Error<string>>.Err(new Error<string>(ErrorKind.TimedOut,
+                "forgot password timed out"));
+        }
+        catch (Exception e)
+        {
+            string message = $"forgot password failed: {e}";
+            _logger.LogInformation(message);
+            return Result<Empty, Error<string>>.Err(new Error<string>(ErrorKind.ServiceError, message));
+        }
     }
 
     public async Task<Result<LoginSuccessPayload, Error<string>>> RefreshToken(string refreshToken)
     {
-        RefreshTokenPayload payload = new()
+        try
         {
-            grant_type = RefreshTokenGrant,
-            client_id = _clientId,
-            client_secret = _clientSecret,
-            refresh_token = refreshToken
-        };
+            RefreshTokenPayload payload = new()
+            {
+                grant_type = RefreshTokenGrant,
+                client_id = _clientId,
+                client_secret = _clientSecret,
+                refresh_token = refreshToken
+            };
 
-        HttpResponseMessage response = await _client.PostAsync($"{_identityProviderUrl}{RefreshTokenEndpoint}",
-            JsonContent.Create(payload));
+            HttpResponseMessage response = await _client.PostAsync($"{_identityProviderUrl}{RefreshTokenEndpoint}",
+                JsonContent.Create(payload));
 
-        if (response.StatusCode != HttpStatusCode.OK)
-        {
-            LoginErrorResponse error = await response.Content.ReadFromJsonAsync<LoginErrorResponse>();
-            return Result<LoginSuccessPayload, Error<string>>.Err(new Error<string>(error.error,
-                error.error_description));
+            if (response.StatusCode != HttpStatusCode.OK)
+            {
+                LoginErrorResponse error = await response.Content.ReadFromJsonAsync<LoginErrorResponse>();
+                return Result<LoginSuccessPayload, Error<string>>.Err(new Error<string>(error.error,
+                    error.error_description));
+            }
+
+            LoginOkResponse successPayload = await response.Content.ReadFromJsonAsync<LoginOkResponse>();
+            return Result<LoginSuccessPayload, Error<string>>.Ok(successPayload);
         }
-
-        LoginOkResponse successPayload = await response.Content.ReadFromJsonAsync<LoginOkResponse>();
-        return Result<LoginSuccessPayload, Error<string>>.Ok(successPayload);
+        catch (TimeoutException)
+        {
+            return Result<LoginSuccessPayload, Error<string>>.Err(new Error<string>(ErrorKind.TimedOut,
+                "refresh token timed out"));
+        }
+        catch (Exception e)
+        {
+            string message = $"refresh token failed: {e}";
+            _logger.LogInformation(message);
+            return Result<LoginSuccessPayload, Error<string>>.Err(new Error<string>(ErrorKind.ServiceError, message));
+        }
     }
 
     private async Task<Result<string, Error<string>>> HandleRegisterAuth0ErrorResponse(HttpResponseMessage response)
